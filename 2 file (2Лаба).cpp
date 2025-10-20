a471a48ca11240635cd3c8e4c9ca6b9806aaba88
Gleb Korenkov K. <konkorvic.ru@gmail.com>
2025-10-21 01:34:29 +0300

    Create 2 file (2Лаба).cpp
    2 лаба 2 файл, праельный вариант

---
+++ 2 file (2Лаба).cpp
@@ -0,0 +1,283 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+class Money {
+private:
+    static const int R_COUNT = 9;  // количество номиналов рублей
+    static const int K_COUNT = 4;  // количество номиналов копеек
+    
+    int r_nom[9];  // номиналы рублей
+    int r_cnt[9];  // количество купюр каждого номинала
+    
+    double k_nom[4];  // номиналы копеек
+    int k_cnt[4];     // количество монет каждого номинала
+    
+    double total;     // общая сумма
+    
+    void normalize(double amt);  // нормализация суммы по номиналам
+
+public:
+    // Конструкторы
+    Money();
+    Money(double amt);
+    Money(const Money& other);
+    
+    // Перегрузка арифметических операторов
+    Money operator+(const Money& other) const;  // Сложение
+    Money operator-(const Money& other) const;  // Вычитание
+    double operator/(const Money& other) const; // Деление сумм
+    Money operator/(double num) const;          // Деление на число
+    Money operator*(double num) const;          // Умножение на число
+    
+    // Перегрузка операторов сравнения
+    bool operator==(const Money& other) const;  // Равенство
+    bool operator!=(const Money& other) const;  // Неравенство
+    bool operator>(const Money& other) const;   // Больше
+    bool operator<(const Money& other) const;   // Меньше
+    
+    // Методы для работы
+    void init(double amt);
+    void read();
+    void display() const;
+    string toString() const;
+    
+    // Дружественная функция для вывода
+    friend ostream& operator<<(ostream& os, const Money& money);
+};
+
+// Реализация методов ВНЕ класса
+
+// Конструктор без аргументов
+Money::Money() : total(0) {
+    int temp_r_nom[9] = {5000, 1000, 500, 100, 50, 10, 5, 2, 1};
+    double temp_k_nom[4] = {0.5, 0.1, 0.05, 0.01};
+    
+    for (int i = 0; i < R_COUNT; i++) {
+        r_nom[i] = temp_r_nom[i];
+        r_cnt[i] = 0;
+    }
+    for (int i = 0; i < K_COUNT; i++) {
+        k_nom[i] = temp_k_nom[i];
+        k_cnt[i] = 0;
+    }
+}
+
+// Конструктор инициализации из double
+Money::Money(double amt) {
+    int temp_r_nom[9] = {5000, 1000, 500, 100, 50, 10, 5, 2, 1};
+    double temp_k_nom[4] = {0.5, 0.1, 0.05, 0.01};
+    
+    for (int i = 0; i < R_COUNT; i++) {
+        r_nom[i] = temp_r_nom[i];
+    }
+    for (int i = 0; i < K_COUNT; i++) {
+        k_nom[i] = temp_k_nom[i];
+    }
+    
+    normalize(amt);
+}
+
+// Конструктор копирования
+Money::Money(const Money& other) : total(other.total) {
+    for (int i = 0; i < R_COUNT; i++) {
+        r_nom[i] = other.r_nom[i];
+        r_cnt[i] = other.r_cnt[i];
+    }
+    for (int i = 0; i < K_COUNT; i++) {
+        k_nom[i] = other.k_nom[i];
+        k_cnt[i] = other.k_cnt[i];
+    }
+}
+
+// Нормализация суммы по номиналам
+void Money::normalize(double amt) {
+    total = amt;
+    
+    for (int i = 0; i < R_COUNT; i++) r_cnt[i] = 0;
+    for (int i = 0; i < K_COUNT; i++) k_cnt[i] = 0;
+    
+    int rub = (int)amt;
+    for (int i = 0; i < R_COUNT; i++) {
+        r_cnt[i] = rub / r_nom[i];
+        rub %= r_nom[i];
+    }
+    
+    int kop = (int)((amt - (int)amt) * 100 + 0.5);
+    for (int i = 0; i < K_COUNT; i++) {
+        int k_val = (int)(k_nom[i] * 100);
+        k_cnt[i] = kop / k_val;
+        kop %= k_val;
+    }
+}
+
+// Основные методы
+void Money::init(double amt) {
+    normalize(amt);
+}
+
+void Money::read() {
+    double amt;
+    cout << "Введите сумму: ";
+    cin >> amt;
+    normalize(amt);
+}
+
+void Money::display() const {
+    cout << "Сумма: " << total << " руб." << endl;
+    cout << "Состав:" << endl;
+    
+    cout << "Рубли: ";
+    bool has_r = false;
+    for (int i = 0; i < R_COUNT; i++) {
+        if (r_cnt[i] > 0) {
+            cout << r_nom[i] << " руб.: " << r_cnt[i] << " шт.; ";
+            has_r = true;
+        }
+    }
+    if (!has_r) cout << "нет";
+    cout << endl;
+    
+    cout << "Копейки: ";
+    bool has_k = false;
+    for (int i = 0; i < K_COUNT; i++) {
+        if (k_cnt[i] > 0) {
+            cout << k_nom[i] << " коп.: " << k_cnt[i] << " шт.; ";
+            has_k = true;
+        }
+    }
+    if (!has_k) cout << "нет";
+    cout << endl;
+}
+
+string Money::toString() const {
+    string str = to_string(total);
+    return str.substr(0, str.find('.') + 3) + " руб.";
+}
+
+// Перегрузка арифметических операторов
+
+Money Money::operator+(const Money& other) const {
+    Money res;
+    res.normalize(this->total + other.total);
+    return res;
+}
+
+Money Money::operator-(const Money& other) const {
+    if (this->total < other.total) {
+        throw "Ошибка: отрицательная сумма!";
+    }
+    Money res;
+    res.normalize(this->total - other.total);
+    return res;
+}
+
+double Money::operator/(const Money& other) const {
+    if (other.total == 0) {
+        throw "Ошибка: деление на ноль!";
+    }
+    return this->total / other.total;
+}
+
+Money Money::operator/(double num) const {
+    if (num == 0) {
+        throw "Ошибка: деление на ноль!";
+    }
+    Money res;
+    res.normalize(this->total / num);
+    return res;
+}
+
+Money Money::operator*(double num) const {
+    Money res;
+    res.normalize(this->total * num);
+    return res;
+}
+
+// Перегрузка операторов сравнения
+
+bool Money::operator==(const Money& other) const {
+    return fabs(this->total - other.total) < 0.001;
+}
+
+bool Money::operator!=(const Money& other) const {
+    return !(*this == other);
+}
+
+bool Money::operator>(const Money& other) const {
+    return this->total > other.total;
+}
+
+bool Money::operator<(const Money& other) const {
+    return this->total < other.total;
+}
+
+// Дружественная функция для вывода
+ostream& operator<<(ostream& os, const Money& money) {
+    os << money.toString();
+    return os;
+}
+
+int main() {
+    cout << "=== Лабораторная работа №2 (Перегрузка операций) ===" << endl;
+    
+    // Демонстрация конструкторов
+    cout << "1. Тестирование конструкторов:" << endl;
+    Money m1;           // Конструктор без аргументов
+    Money m2(150.75);   // Конструктор инициализации
+    Money m3(m2);       // Конструктор копирования
+    
+    cout << "m1: " << m1 << endl;
+    cout << "m2: " << m2 << endl;
+    cout << "m3: " << m3 << endl;
+    
+    // Демонстрация перегруженных операторов
+    cout << "\n2. Арифметические операции с перегрузкой:" << endl;
+    
+    Money sum = m2 + m3;
+    cout << "m2 + m3 = " << sum << endl;
+    
+    Money diff = m2 - m1;
+    cout << "m2 - m1 = " << diff << endl;
+    
+    double divVal = m2 / m3;
+    cout << "m2 / m3 = " << divVal << endl;
+    
+    Money divNum = m2 / 3;
+    cout << "m2 / 3 = " << divNum << endl;
+    
+    Money mulNum = m2 * 2;
+    cout << "m2 * 2 = " << mulNum << endl;
+    
+    // Демонстрация перегруженных операторов сравнения
+    cout << "\n3. Операции сравнения с перегрузкой:" << endl;
+    cout << "m2 == m3: " << (m2 == m3 ? "да" : "нет") << endl;
+    cout << "m2 != m1: " << (m2 != m1 ? "да" : "нет") << endl;
+    cout << "m2 > m1: " << (m2 > m1 ? "да" : "нет") << endl;
+    cout << "m2 < m3: " << (m2 < m3 ? "да" : "нет") << endl;
+    
+    // Демонстрация работы с массивами
+    cout << "\n4. Массив объектов:" << endl;
+    Money arr[3] = { Money(50.25), Money(100.50), Money(200.75) };
+    for (int i = 0; i < 3; i++) {
+        cout << "arr[" << i << "]: " << arr[i] << endl;
+    }
+    
+    // Демонстрация сложных выражений
+    cout << "\n5. Сложные выражения:" << endl;
+    Money test1(10.10);
+    Money test2(5.05);
+    Money test3 = test1 + test2;
+    cout << "10.10 + 5.05 = " << test3 << endl;
+    
+    Money test4 = test1 - test2;
+    cout << "10.10 - 5.05 = " << test4 << endl;
+    
+    // Комбинированные операции показывают преимущества перегрузки
+    Money complex = (test1 + test2) * 2 - Money(5.00);
+    cout << "(10.10 + 5.05) * 2 - 5.00 = " << complex << endl;
+
+    return 0;
+}
