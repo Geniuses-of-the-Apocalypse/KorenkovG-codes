5d7cf23096c17598febc75bdb7e0fce08480b970
Gleb Korenkov K. <konkorvic.ru@gmail.com>
2025-11-28 22:20:12 +0300

    Update 11 file (1Лаба).cpp
    Готовая лаба

--- 11 file (1Лаба).cpp
+++ 11 file (1Лаба).cpp
@@ -1,1 +1,33 @@
+#include <iostream>
 
+using namespace std;
+
+// Constexpr лямбда для сортировки массива
+template<typename T, size_t N>
+constexpr auto sortArray = [](T arr[N]) {
+    // Сортировка пузырьком
+    for (size_t i = 0; i < N - 1; i++)
+        for (size_t j = 0; j < N - i - 1; j++)
+            if (arr[j] > arr[j + 1]) {
+                T temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+};
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    
+    // Тестируем сортировку
+    int arr[] = {5, 2, 8, 1, 9};
+    
+    cout << "Исходный массив: ";
+    for (int num : arr) cout << num << " ";
+    
+    sortArray<int, 5>(arr);
+    
+    cout << "\nОтсортированный: ";
+    for (int num : arr) cout << num << " ";
+    
+    return 0;
+}
