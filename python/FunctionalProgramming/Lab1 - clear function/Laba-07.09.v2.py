147924fd0695ee1cdd49716f1868b2e1893ecca6
Gleb Korenkov K. <konkorvic.ru@gmail.com>
2026-09-21 11:14:06 +0300

    1 лабораторная работа, но через рекурсию- чистые функции. Сделали без rows = parse_csv(CSV_DATA), а просто через parse_csv(CSV_DATA).
    1 лабораторная работа, но через рекурсию - чистые функции.
    Сделали без rows = parse_csv(CSV_DATA), а просто через parse_csv(CSV_DATA).

--- python/FunctionalProgramming/Lab1 - clear function/Laba-07.09.v2.py
+++ python/FunctionalProgramming/Lab1 - clear function/Laba-07.09.v2.py
@@ -24,17 +24,15 @@ def top_item(rows: list[dict]) -> Optional[dict]:
     f = lambda x, rest: x if not rest else f(x if rev(x) > rev(rest[0]) else rest[0], rest[1:])
     return f(rows[0], rows[1:])
 
-# main
-rows = parse_csv(CSV_DATA)
 
 print("Данные: ")
-print(rows)
+print(parse_csv(CSV_DATA))
 
-print("\n Выручка: ", compute_revenue(rows))
+print("\n Выручка: ", compute_revenue(parse_csv(CSV_DATA)))
 
 print("\n Товар с максимальной выручкой: ")
-print(top_item(rows))
-print(f"Сумма выручки этого товара: {float(top_item(rows)['quantity']) * float(top_item(rows)['price'])}")
+print(top_item(parse_csv(CSV_DATA)))
+print(f"Сумма выручки этого товара: {float(top_item(parse_csv(CSV_DATA))['quantity']) * float(top_item(parse_csv(CSV_DATA))['price'])}")
 
 # 4
 #
